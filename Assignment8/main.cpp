952f67b7f25d27bc480493f7ef8e370e934c5957
AThreeDimensionalLemon <eisigliang@gmail.com>
2025-03-04 15:20:38 -0800

    Add project files.

---
+++ Assignment8/main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+static int prog2() {
+    return 0;
+}
+
+static int prog4() {
+    return 0;
+}
+
+static int prog5() {
+    return 0;
+}
+
+static int prog6() {
+    return 0;
+}
+
+int main() {
+    std::cout << "Eisig Liang - 9 Mar. 2025\n";
+    return 0;
+}
