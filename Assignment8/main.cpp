6df54b68d977868593a04ce7ece0ac825897b299
AThreeDimensionalLemon <eisigliang@gmail.com>
2025-03-06 17:22:46 -0800

    Implemented Prog4 part of assignmnet

--- Assignment8/main.cpp
+++ Assignment8/main.cpp
@@ -10,8 +10,15 @@ static int prog2(int counter) {
     return 0;
 }
 
-static int prog4() {
-    return 0;
+static int prog4(int integer) {
+    int result = 1;
+
+    while (integer > 0) {
+        result *= integer;
+        integer--;
+    }
+
+    return result; //Professor likely didn't mean to tell me to just return 1
 }
 
 static int prog5() {
@@ -23,7 +30,12 @@ static int prog6() {
 }
 
 int main() {
-    std::cout << "Eisig Liang - 9 Mar. 2025\n";
-    prog2(12);
+    std::cout << "Eisig Liang - 9 Mar. 2025\n\nTesting prog2: ";
+    //prog2(12);
+
+    std::cout << "Skipped\n\nTesting prog4: ";
+    std::cout << prog4(8) << std::endl;
+
+    std::cout << "Complete\n\nTesting prog 5: Skipped";
     return 0;
 }
